cb9a8f244c4ccc74136c62d75af4f8bf5a374cfe
MJL <maciekjl1993@gmail.com>
2021-11-09 20:16:24 +0100

    to jest cwiczenie15 v1

---
+++ zadanie15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace zadanie15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Podaj liczbę całkowitą większą lub równą 0");
+            int potega = int.Parse(Console.ReadLine());
+            int bufor = 1;
+            int licznik = 1;
+            while(potega<0)
+            {
+                Console.Clear();
+                Console.WriteLine("Podałeś nieprawidłową liczbę");
+                Console.WriteLine("Podaj liczbę całkowitą większą lub równą 0");
+                potega = int.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+            if (potega == 0)
+            {
+                Console.WriteLine("2 do potęgi 0 wynosi: \t 1");
+            }
+            else
+            {
+                Console.WriteLine("2 do potęgi 0 wynosi: \t 1");
+                while (licznik < potega + 1)
+                {
+                    bufor = bufor * 2;
+                    Console.WriteLine($"2 do potęgi {licznik} wynosi: \t {bufor}");
+                    licznik++;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
